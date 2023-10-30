447d699bfc6f7ef7797ad193b2fa876778ed2eb4
Cong Hao <conghao.hc@gmail.com>
2023-10-30 21:23:26 +0700

    update

--- QuanLychiTieu/Statistics.Designer.cs
+++ QuanLychiTieu/Statistics.Designer.cs
@@ -28,50 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title3 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.chartMain = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.lkExpenses = new System.Windows.Forms.LinkLabel();
+            this.cbFill = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.lkIncome = new System.Windows.Forms.LinkLabel();
+            this.lkExpenses = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.chartMain)).BeginInit();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // chartMain
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chartMain.ChartAreas.Add(chartArea1);
-            legend1.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
-            legend1.Name = "Legend1";
-            legend1.Title = "Statistics";
-            this.chartMain.Legends.Add(legend1);
+            chartArea3.Name = "ChartArea1";
+            this.chartMain.ChartAreas.Add(chartArea3);
+            legend3.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
+            legend3.Name = "Legend1";
+            legend3.Title = "Statistics";
+            this.chartMain.Legends.Add(legend3);
             this.chartMain.Location = new System.Drawing.Point(12, 115);
             this.chartMain.Name = "chartMain";
-            series1.ChartArea = "ChartArea1";
-            series1.EmptyPointStyle.Name = "Expenses";
-            series1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            series1.IsValueShownAsLabel = true;
-            series1.Legend = "Legend1";
-            series1.Name = "Expenses";
-            series2.ChartArea = "ChartArea1";
-            series2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            series2.IsValueShownAsLabel = true;
-            series2.Legend = "Legend1";
-            series2.Name = "Income";
-            this.chartMain.Series.Add(series1);
-            this.chartMain.Series.Add(series2);
+            series5.ChartArea = "ChartArea1";
+            series5.EmptyPointStyle.Name = "Expenses";
+            series5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series5.IsValueShownAsLabel = true;
+            series5.Legend = "Legend1";
+            series5.Name = "Expenses";
+            series6.ChartArea = "ChartArea1";
+            series6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series6.IsValueShownAsLabel = true;
+            series6.Legend = "Legend1";
+            series6.Name = "Income";
+            this.chartMain.Series.Add(series5);
+            this.chartMain.Series.Add(series6);
             this.chartMain.Size = new System.Drawing.Size(669, 473);
             this.chartMain.TabIndex = 0;
             this.chartMain.Text = "chart1";
-            title1.Name = "Statistics";
-            this.chartMain.Titles.Add(title1);
+            title3.Name = "Statistics";
+            this.chartMain.Titles.Add(title3);
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.cbFill);
+            this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.lkIncome);
             this.groupBox1.Controls.Add(this.lkExpenses);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -80,14 +87,48 @@
             this.groupBox1.Size = new System.Drawing.Size(677, 100);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Filler";
+            this.groupBox1.Text = "Tools";
+            // 
+            // cbFill
+            // 
+            this.cbFill.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFill.FormattingEnabled = true;
+            this.cbFill.Location = new System.Drawing.Point(148, 25);
+            this.cbFill.Name = "cbFill";
+            this.cbFill.Size = new System.Drawing.Size(246, 24);
+            this.cbFill.TabIndex = 3;
+            this.cbFill.SelectedIndexChanged += new System.EventHandler(this.cbFill_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(86, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Fill:";
+            // 
+            // lkIncome
+            // 
+            this.lkIncome.AutoSize = true;
+            this.lkIncome.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lkIncome.LinkColor = System.Drawing.Color.Gray;
+            this.lkIncome.Location = new System.Drawing.Point(278, 78);
+            this.lkIncome.Name = "lkIncome";
+            this.lkIncome.Size = new System.Drawing.Size(57, 16);
+            this.lkIncome.TabIndex = 1;
+            this.lkIncome.TabStop = true;
+            this.lkIncome.Text = "Income";
+            this.lkIncome.MouseEnter += new System.EventHandler(this.lkIncome_MouseEnter);
+            this.lkIncome.MouseLeave += new System.EventHandler(this.lkIncome_MouseLeave);
             // 
             // lkExpenses
             // 
             this.lkExpenses.AutoSize = true;
             this.lkExpenses.Cursor = System.Windows.Forms.Cursors.Hand;
             this.lkExpenses.LinkColor = System.Drawing.Color.Gray;
-            this.lkExpenses.Location = new System.Drawing.Point(48, 81);
+            this.lkExpenses.Location = new System.Drawing.Point(142, 78);
             this.lkExpenses.Name = "lkExpenses";
             this.lkExpenses.Size = new System.Drawing.Size(75, 16);
             this.lkExpenses.TabIndex = 0;
@@ -96,19 +137,15 @@
             this.lkExpenses.MouseEnter += new System.EventHandler(this.lkExpenses_MouseEnter);
             this.lkExpenses.MouseLeave += new System.EventHandler(this.lkExpenses_MouseLeave);
             // 
-            // lkIncome
+            // pictureBox1
             // 
-            this.lkIncome.AutoSize = true;
-            this.lkIncome.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.lkIncome.LinkColor = System.Drawing.Color.Gray;
-            this.lkIncome.Location = new System.Drawing.Point(129, 81);
-            this.lkIncome.Name = "lkIncome";
-            this.lkIncome.Size = new System.Drawing.Size(57, 16);
-            this.lkIncome.TabIndex = 1;
-            this.lkIncome.TabStop = true;
-            this.lkIncome.Text = "Income";
-            this.lkIncome.MouseEnter += new System.EventHandler(this.lkIncome_MouseEnter);
-            this.lkIncome.MouseLeave += new System.EventHandler(this.lkIncome_MouseLeave);
+            this.pictureBox1.Image = global::QuanLychiTieu.Properties.Resources.financial_statement;
+            this.pictureBox1.Location = new System.Drawing.Point(500, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(124, 83);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
             // 
             // Statistics
             // 
@@ -125,6 +162,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.chartMain)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -135,5 +173,8 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.LinkLabel lkIncome;
         private System.Windows.Forms.LinkLabel lkExpenses;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbFill;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- QuanLychiTieu/Statistics.cs
+++ QuanLychiTieu/Statistics.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
 
 namespace QuanLychiTieu
 {
@@ -15,10 +16,18 @@ namespace QuanLychiTieu
     {
         private QLChiTieuModel _qLChiTieu;
         private int _userId;
+        private List<ComboItem> _comboItems;
         public Statistics(int userId)
         {
             InitializeComponent();
             _userId = userId;
+            _comboItems = new List<ComboItem> {
+            new ComboItem{ Value = 1, Display = "This week"},
+            new ComboItem{ Value = 2, Display = "This month"},
+            new ComboItem{ Value = 3, Display = "This year"},
+            new ComboItem{ Value = 4, Display = "This last 5 years"}
+            };
+
         }
 
         private void Statistics_Load(object sender, EventArgs e)
@@ -26,7 +35,9 @@ namespace QuanLychiTieu
             _qLChiTieu = new QLChiTieuModel();
             var expenses = _qLChiTieu.EXPENSES.Where(x => x.USERID == _userId)
                            .GroupBy(x => x.EXDATE.Value.Year)
-                           .Select(x => new {date = x.Key, money = x.Sum(m => m.MONEY) })
+                           .Select(x => new { date = x.Key, money = x.Sum(m => m.MONEY) })
+                           .OrderByDescending(x => x.date)
+                           .Take(5)
                            .ToList();
             foreach (var item in expenses)
             {
@@ -34,11 +45,19 @@ namespace QuanLychiTieu
             }
             var income = _qLChiTieu.INCOMEs.Where(x => x.USERID == _userId)
                          .GroupBy(x => x.INDATE.Value.Year)
-                         .Select(x => new { date = x.Key, money = x.Sum(m => m.MONEY) });
-            foreach(var item in income)
+                         .Select(x => new { date = x.Key, money = x.Sum(m => m.MONEY) })
+                         .OrderByDescending(x => x.date)
+                         .Take(5)
+                         .ToList();
+            foreach (var item in income)
             {
                 chartMain.Series["Income"].Points.AddXY(item.date, (double)item.money);
             }
+            cbFill.SelectedIndexChanged -= cbFill_SelectedIndexChanged;
+            cbFill.DataSource = _comboItems;
+            cbFill.ValueMember = "Value";
+            cbFill.DisplayMember = "Display";
+            cbFill.SelectedIndexChanged += cbFill_SelectedIndexChanged;
         }
 
         private void lkExpenses_MouseEnter(object sender, EventArgs e)
@@ -59,6 +78,45 @@ namespace QuanLychiTieu
         private void lkIncome_MouseLeave(object sender, EventArgs e)
         {
             lkIncome.LinkColor = Color.Gray;
+        }
+
+        private void cbFill_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var selectItem = cbFill.SelectedValue;
+            switch (selectItem)
+            {
+                case 1:
+                    chartMain.Series["Expenses"].Points.Clear();
+                    chartMain.Series["Income"].Points.Clear();
+                    string date = DateTime.Now.ToShortDateString();
+                    string sql = "SELECT EXDATE AS \"_date\", SUM(MONEY) AS \"_money\" FROM EXPENSES WHERE USERID = :p0 AND " +
+                                 "EXDATE >= TRUNC(TO_DATE(:p1, 'DD-MM-YYYY'), 'IW') AND " +
+                                 "EXDATE <= TRUNC(TO_DATE(:p1, 'DD-MM-YYYY'), 'IW') + 6 " +
+                                 "GROUP BY EXDATE";
+                    var expenses = _qLChiTieu.Database.SqlQuery<ResultDB>(sql, _userId, date);
+                    foreach (var item in expenses)
+                    {
+                        chartMain.Series["Expenses"].Points.AddXY(item._date.ToShortDateString(), (double)item._money);
+                    }
+                    sql = "SELECT INDATE AS \"_date\", SUM(MONEY) AS \"_money\" FROM INCOME WHERE USERID = :p0 AND " +
+                           "INDATE >= TRUNC(TO_DATE(:p1, 'DD-MM-YYYY'), 'IW') AND " +
+                           "INDATE <= TRUNC(TO_DATE(:p1, 'DD-MM-YYYY'), 'IW') + 6 " +
+                           "GROUP BY INDATE";
+                    var income = _qLChiTieu.Database.SqlQuery<ResultDB>(sql, _userId, date);
+                    foreach (var item in expenses)
+                    {
+                        chartMain.Series["Income"].Points.AddXY(item._date.ToShortDateString(), (double)item._money);
+                    }
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
